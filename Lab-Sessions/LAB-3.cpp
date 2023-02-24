fa82526587440ec506fd83b25bfcf4df605fa307
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-02-24 23:42:44 +0000

    Change name of base file of lab 3a to lab 3

--- Lab-Sessions/LAB-3.cpp
+++ Lab-Sessions/LAB-3.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 
-int sumNum(int n) {
-    return ((n*n)+n)/2;
-}
-
-int main() {
+void summationUpToN() {
     int N;
 
     std::cout << "Hey!!! I find the sum of n natural numbers!\n\n";
 
     system("pause=2"); system("cls");
-    
+
     std::cout << "Enter N: ";
     std::cin >> N;
+    
+    std::cout << "The sum of numbers up to " << N << " is: " << ((N*N) + N) / 2 << std::endl;
+}
 
-    std::cout << "The sum of numbers up to " << N << " is: " << sumNum(N) << std::endl;
+int main() {
+    
 
     return 0;
 }
