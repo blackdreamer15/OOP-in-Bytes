f45dec2b186b627cb28565e58304c79cb6fa2215
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-02-25 18:07:48 +0000

    Add changes to base file of lab session 3

--- Lab-Sessions/LAB-3.cpp
+++ Lab-Sessions/LAB-3.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
 
-void summationUpToN() {
+int summationUpToN(int N) {
+    return ((N*N) + N) / 2;
+}
+
+float convertHeight_CmToFeet(int heightInCm) {
+    return heightInCm / (12*2.54);
+}
+
+int fibonacciSequence(int N) {
+    int firstTerm = 0, secondTerm = 1, nextTerm, counter = 1;
+    std::cout << "Fibonacci Sequence: ";
+
+    while(nextTerm <= N) {
+        if (counter == 1) {
+            std::cout << firstTerm << "  ";
+            counter++;
+            continue;
+        }
+        else if (counter == 2) {
+            std::cout << secondTerm << "  ";
+            counter++;
+            continue;
+        }
+
+        nextTerm = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = nextTerm;
+
+        if (nextTerm <= N) {
+            std::cout << nextTerm << "  ";
+            counter++;
+        }
+    }
+}
+
+
+void summationUpToN_Run() {
     int N;
 
     std::cout << "Hey!!! I find the sum of n natural numbers!\n\n";
@@ -10,11 +46,38 @@ void summationUpToN() {
     std::cout << "Enter N: ";
     std::cin >> N;
     
-    std::cout << "The sum of numbers up to " << N << " is: " << ((N*N) + N) / 2 << std::endl;
+    std::cout << "The sum of numbers up to " << N << " is: " << summationUpToN(N) << std::endl;
+}
+
+void convertHeight_CmToFeet_Run() {
+    float height_cm, height_ft;
+    
+    std::cout << "Enter your height in centimeters: ";
+    std::cin >> height_cm;
+
+    height_ft = convertHeight_CmToFeet(height_cm);
+
+    std::cout << "Your height is: " << height_ft << "feet and " << height_ft - (int)height_ft << "inches.";
+}
+
+void fibonacciSequence_Run() {
+    int N;
+
+    std::cout << "=== FIBONACCI SEQUENCE ===\n";
+    std::cout << "Enter the number up to which you want to generate the Fibonacci Sequence: ";
+    std::cin >> N;
+
+    fibonacciSequence(N);
 }
 
 int main() {
-    
+    summationUpToN_Run();
+    std::cout << "\n\n=========================\n\n";
+
+    convertHeight_CmToFeet_Run();
+    std::cout << "\n\n=========================\n\n";
+
+    fibonacciSequence_Run();
 
     return 0;
 }
