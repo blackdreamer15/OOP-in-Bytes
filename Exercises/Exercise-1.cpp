86e1c300478a8131b21e742373fcb756af726797
Baki Jessy Justice Julien <bakijessy@gmail.com>
2023-03-04 18:20:17 +0000

    Update - Add preamble for the code

--- Exercises/Exercise-1.cpp
+++ Exercises/Exercise-1.cpp
@@ -1,3 +1,9 @@
+/*
+Assuming there are 7.481 gallons in a cubic foot, write a program that asks 
+the user to enter a number of gallons, and then displays the equivalent in 
+cubic feet.
+*/
+
 #include <iostream>
 
 inline double gallonsToCubicFeet(double gallons) {
